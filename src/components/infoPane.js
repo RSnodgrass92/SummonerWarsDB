06a32884d9acc83ce0865bee7f96135b8161b66b
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-09-01 17:16:29 -0600

    mobile view edits

--- src/components/infoPane.js
+++ src/components/infoPane.js
@@ -52,36 +52,36 @@ class InfoPane extends Component
 
 
                 {/* mobile */}
-                <div className="container d-lg-none">
+                <div className="container d-lg-none" id="mobile-content-wrapper">
                     <div className="row">
-                        <h1 className="col sub-title">{factionObj.name}</h1>
+                        <h1 className="col mobile-sub-title">{factionObj.name}</h1>
                     </div>
                     <div className="row">
-                        <img className=" col-12 img-fluid" src={factionObj.summoner}/>
+                        <img className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.summoner}/>
                         <hr></hr>
                     </div>
                     <div className="row">
-                        <h3 className="col sub-title">Champions</h3>
+                        <h3 className="col mobile-sub-title">Champions</h3>
                     </div>
                     <div className="row">
-                    <img className=" col-12 img-fluid" src={factionObj.champions[0]}/>
-                    <img className=" col-12 img-fluid" src={factionObj.champions[1]}/>
+                    <img className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.champions[0]}/>
+                    <img className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.champions[1]}/>
                     </div>
                     <div className="row">
-                    <img className=" col-12 img-fluid" src={factionObj.champions[2]}/>
+                    <img className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.champions[2]}/>
                     </div>
                     <hr></hr>
                     <div className="row">
-                    <h3 className="col sub-title">Common Units</h3>
+                    <h3 className="col mobile-sub-title">Common Units</h3>
                     </div>
                     <div className="row">
-                        {factionObj.units.map(element => <img key={factionObj.units.indexOf(element)} className=" col-12 img-fluid" src={factionObj.units[factionObj.units.indexOf(element)]}/>)}
+                        {factionObj.units.map(element => <img key={factionObj.units.indexOf(element)} className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.units[factionObj.units.indexOf(element)]}/>)}
                     </div>
                     <hr></hr>
                     <div className="row">
-                        <h3 className="col-12 sub-title">Events</h3>
+                        <h3 className="col-12 mobile-sub-title">Events</h3>
                     </div>
-                    {factionObj.events.map(element => <img key={factionObj.events.indexOf(element)} className=" col-12 img-fluid" src={factionObj.events[factionObj.events.indexOf(element)]}/>)}
+                    {factionObj.events.map(element => <img key={factionObj.events.indexOf(element)} className=" col-12 img-fluid mobile-img-wrapper" src={factionObj.events[factionObj.events.indexOf(element)]}/>)}
                 </div>
 
                 </>
